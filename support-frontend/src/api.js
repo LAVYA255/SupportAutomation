406b439fe6dc48a181a39f8f407cac04349144c5
Lav <lavya333@gmail.com>
2026-02-17 01:14:52 +0530

    Link update

--- support-frontend/src/api.js
+++ support-frontend/src/api.js
@@ -7,7 +7,7 @@ const HOST =
 const API =
 	typeof window !== "undefined" && window.location.hostname === "localhost"
 		? `http://${HOST}:8000`
-		: "https://supportautomation.onrender.com"
+		: "https://support-automation.onrender.com"
 export const UNAUTHORIZED_EVENT = "support-unauthorized"
 
 const client = axios.create({
